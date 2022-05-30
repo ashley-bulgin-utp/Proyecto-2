ced4fc4fd1a1c003d4961571c187efb990962f5f
lokzar88 <106524878+lokzar88@users.noreply.github.com>
2022-05-30 16:54:52 -0500

    Update parte2.py
    cambios para usuario de la ecuacion y nueva funcion para que el programa saque la derivada por si mismo

--- parte2.py
+++ parte2.py
@@ -1,32 +1,39 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sympy import Symbol
+import numpy as np #libreria para proceso matematicos
+import matplotlib.pyplot as plt #libreria para la grafica
+from sympy import Symbol #libreria para calcular la derivada
+import sympy
 
-
-fx = lambda x: (x**3)-2*x-5
-dx = lambda x: 3*(x**2)-2
-xi= 0
-x = Symbol('x')
-y = (x**3)-2*x-5
-dxx =y.diff(x)
-print(dxx)
-
+#creacion de variables
+xi= 0 #variables para inicializar los calculos
+x = Symbol('x') #Crea la Variable X
 tolerancia = 0.00001
 error = 1
 
+
+f = input("Digite una funcion (con variable x): ")
+df = sympy.diff(f) #Deriva a la funcion F
+
+print("La derivada de la funcion es : ",df)
+
+f= sympy.lambdify(x, f) 
+df= sympy.lambdify(x, df)
+
+i = 0 #contador para iteracion
 while error > tolerancia:
-  raiz = xi - (fx(xi)/dx(xi))
+  i+=1
+  raiz = xi - (f(xi)/df(xi))
   error = np.abs((raiz-xi)/raiz)
+  print('x ',i, '= ', xi) #Imprime las iteraciones para ver cuantas se tuvo que hacer
   xi = raiz
 
-print("la raiz es: ",raiz)
-print("y el valor de f(x) es: ", fx(raiz))
+print("La raiz es: ",raiz)
+print("El valor de f(x) es: ", f(raiz))
 
-
-xs = np.linspace(raiz-1, raiz+1, 50)
-plt.plot(xs, fx(xs), label = 'f(x)')
-plt.title("Metodo Newton - Raphson")
-plt.axhline(y=0, color = 'g')
-plt.plot(raiz, fx(raiz),'bo',label = f'f(x)=0, x={raiz:.6f}', color= 'k')
-plt.legend(loc = 'upper left')
-plt.show()
+#lineas de codigo para la grafica
+xs = np.linspace(raiz-1, raiz+1, 50) #evaluar puntos de raiz-1 y raiz+1, 50 es la cantidad de puntos que deseo tener
+plt.plot(xs, f(xs), label = 'f(x)')
+plt.title("Metodo Newton - Raphson") #titulo
+plt.axhline(y=0, color = 'g') #hacer aparecer la linea en eje Y=0
+plt.plot(raiz, f(raiz),'bo',label = f'f(x)=0, x={raiz:.6f}', color= 'k') #comando para ver el punto en la grafica
+plt.legend(loc = 'upper left') #legenda
+plt.show() #mostrar grafica
